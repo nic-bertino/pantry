e36d139f41f282de2fc26956cf561943f989de77
Nic <nic.bertino@gmail.com>
2026-01-15 12:01:15 -0800

    chore: removed Clarity - not intended for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { DM_Sans, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const dmSans = DM_Sans({
@@ -45,22 +44,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className={dmSans.variable}>
-			<body className={`${geistMono.variable} antialiased`}>
-				{children}
-				<Script
-					id="clarity-script"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `
-							(function(c,l,a,r,i,t,y){
-								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-							})(window, document, "clarity", "script", "v0yhu0nkez");
-						`,
-					}}
-				/>
-			</body>
+			<body className={`${geistMono.variable} antialiased`}>{children}</body>
 		</html>
 	);
 }
