de9ab574ff79b13e05a97960afe125b7dab8f262
Nic <nic.bertino@gmail.com>
2026-01-22 11:47:44 -0800

    feat: clarity can be set by optional env variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { DM_Sans, Geist_Mono } from "next/font/google";
+import Script from "next/script";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const dmSans = DM_Sans({
+const geist = Geist({
 	subsets: ["latin"],
 	variable: "--font-sans",
 });
@@ -43,8 +44,26 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={dmSans.variable}>
-			<body className={`${geistMono.variable} antialiased`}>{children}</body>
+		<html lang="en" className={geist.variable}>
+			<body className={`${geistMono.variable} antialiased`}>
+				{children}
+
+				{process.env.CLARITY_PROJECT_ID && (
+					<Script
+						id="clarity-script"
+						strategy="afterInteractive"
+						dangerouslySetInnerHTML={{
+							__html: `
+							(function(c,l,a,r,i,t,y){
+								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "${process.env.CLARITY_PROJECT_ID}");
+						`,
+						}}
+					/>
+				)}
+			</body>
 		</html>
 	);
 }
